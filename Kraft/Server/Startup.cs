775bbc781a57082733569ba4a519047321e6c111
Jiff Shen <Jiff.Shen@wdc.com>
2020-05-27 09:41:56 +0800

    update layout & css for minimized sidebar

--- Kraft/Server/Startup.cs
+++ Kraft/Server/Startup.cs
@@ -52,7 +52,7 @@ namespace Kraft.Server
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
