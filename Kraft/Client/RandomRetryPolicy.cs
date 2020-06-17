0db6d91887165848d58e0e387ab5239c24f166b1
m3l3m01t <m3l3m01t@gmail.com>
2020-06-17 16:58:29 +0800

    add signalr client retrypolicy

---
+++ Kraft/Client/RandomRetryPolicy.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace Kraft.Client
+{
+    public class RandomRetryPolicy : IRetryPolicy
+    {
+        private readonly Random _rand;
+
+        public RandomRetryPolicy()
+        {
+            _rand = new Random();
+        }
+        public TimeSpan? NextRetryDelay(RetryContext retryContext)
+        {
+            return TimeSpan.FromSeconds(_rand.Next(2, 6));
+        }
+    }
+}
